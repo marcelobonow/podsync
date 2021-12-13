248f6699b6918cff6841ea4eefa599b843ba3550
Marcelo Ott <marcelobonowott@hotmail.com>
2021-12-13 01:19:46 -0300

    Get request hello world

--- src/main/java/com/podsync/podsync/controllers/UserController.java
+++ src/main/java/com/podsync/podsync/controllers/UserController.java
@@ -1,15 +1,19 @@
 package com.podsync.podsync.controllers;
 
+import com.podsync.podsync.model.User;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-  @GetMapping("/")
-  public String HelloWorld() {
-    return "Hello World";
+  @GetMapping("")
+  public User HelloWorld() {
+    var user = new User((long)15, "Teste", "Teste");
+    return user;
   }
 }
---
+++ src/main/java/com/podsync/podsync/model/User.java
@@ -0,0 +1,14 @@
+package com.podsync.podsync.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+public class User {
+  private Long id;
+  private String name;
+  private String userName;
+
+}
