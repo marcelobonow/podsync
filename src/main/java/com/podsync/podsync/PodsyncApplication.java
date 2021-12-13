3ec72c3d71d1e3e9a283127e8a4e16f9b483a951
Marcelo Ott <marcelobonowott@hotmail.com>
2021-12-13 00:27:37 -0300

    Config from project using spring initializr

---
+++ src/main/java/com/podsync/podsync/PodsyncApplication.java
@@ -0,0 +1,13 @@
+package com.podsync.podsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/podsync/podsync/PodsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podsync.podsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
